8a4a5041f1e9b34742c5348c8233873e4d877bed
sailist <sailist@outlook.com>
2023-02-18 16:18:48 +0800

    reformat repository

---
+++ extract_version.py
@@ -0,0 +1,5 @@
+from setup import extract_version
+
+if __name__ == '__main__':
+    print(extract_version())
+
--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     install_requires=['dbrecord', 'requests'],
     # https://pypi.org/classifiers/
     classifiers=[
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
