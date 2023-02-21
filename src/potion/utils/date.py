0fff852e8f1fa0cd9e2e2636196786b245901b0c
sailist <sailist@outlook.com>
2023-02-21 16:47:27 +0800

    utils to parse notion date

--- src/potion/utils/date.py
+++ src/potion/utils/date.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 
 
-def strftime(fmt='%y-%m-%d-%H%M%S', dateobj: datetime = None):
+def strftime(fmt='%y-%m-%d-%H%M%S', dateobj: datetime = None) -> str:
     """get current date with formatted"""
     if dateobj is not None:
         return dateobj.strftime(fmt)
     return datetime.now().strftime(fmt)
+
+
+def parse_notion_date(datestr, fmt='%Y-%m-%dT%H:%M:%S.%fZ') -> datetime:
+    return datetime.strptime(datestr, fmt)
